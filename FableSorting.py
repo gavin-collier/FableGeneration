0f448d04911d6eacf485aeaf8190eb9888598411
IrkedCow <gavin.mc.collier@gmail.com>
2021-05-11 12:10:06 -0400

    sorted fables into separate .txt files

--- FableSorting.py
+++ FableSorting.py
@@ -1,12 +1,22 @@
 import string
 
 text = open('Fables.txt', 'r')
+i = 0
+c = 0
+f = open("Fable" + str(c) + ".txt", "x") 
 
 while True:
-      
     # read by character
-    char = text.read(1)          
+    char = text.read(1)
+    if char == '\n':
+        i += 1
+    else:
+        i = 0
+    if i > 4:
+        c += 1
+        f.close
+        f = open("Fable" + str(c) + ".txt", "x")           
     if not char: 
         break
           
-    print(char)
+    f.write(char)
